d5d465a5e058d8cfc11da3df8d35bfe3c510278a
DasturchiQurbonboyev <dasturchiqurbonboyev@gmail.com>
2023-12-09 09:56:13 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,206 @@
+// for 1
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+
+// let sonlar = 0;
+
+// for (let i = a; i <= b; i++) {
+//     sonlar++;
+//     console.log(i);
+// }
+
+// console.log(sonlar);
+
+
+// for 2
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+
+// let sonlar = 0;
+
+// for (let i = b-1; i > a; i--) {
+//     sonlar++;
+//     console.log(i);
+// }
+
+// console.log(sonlar);
+
+// for 3
+
+// let n = +prompt("n ni kiriting");
+// let kg = +prompt("kg ni kiriting");
+
+// let sum;
+
+// for (let i = n; i <= 10 ; i++) {
+    
+//     sum = i*kg;
+//     console.log(sum);
+    
+// }
+
+// for 4
+
+// let kg = +prompt(" kg narxini kiriting");
+
+// for (let i= 12; i<=20; i++){
+//     console.log((kg*i)/10);
+// }
+
+// for 5
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//    if(a<b){
+//      sum +=i;
+//    }
+// }
+
+// console.log(sum);
+
+// for 6
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+
+// let mul = 1;
+
+// for (let i = a; i <= b; i++) {
+//    if(a<b){
+//      mul *=i;
+//    }
+// }
+
+// console.log(mul);
+
+// for 7
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+
+// let S = 0;
+
+// for (let i = a; i <= b; i++) {
+//    if(a<b){
+//      S +=i**2;
+//    }
+// }
+
+// console.log(S);
+
+// for 8
+
+// let n = +prompt(" n ni kiriting")
+
+// let S = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     S += 1 / i;
+// }
+
+// console.log("Natija:", S);
+
+
+// for 9
+
+// let n = +prompt("n ni kiriting (n > 0):");
+// let S = 1;
+
+// for (let i = 1; i <= n; i++) {
+//     S *= 1.1 * i;
+// }
+
+// console.log("Natija:", S);
+
+// for 10
+
+// let n = +prompt("n ni kiriting (n > 0):");
+
+// let S = 0;
+
+// let C = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     S=(2*i-1);
+//     C += S;
+//     console.log(S);
+// }
+
+// console.log(C);
+
+
+// for 11
+
+// let n = +prompt("n ni kiriting (n > 0):");
+// let a = +prompt("a ni kiriting ");
+
+// result =1;
+
+// for (let i = 1; i <= n; i++){
+//     result *= a;
+// }
+
+// console.log(result);
+
+// for 12
+
+// let n = +prompt("n ni kiriting (n > 0):");
+// let a = +prompt("a ni kiriting ");
+
+// result =1;
+
+// for (let i = 1; i <= n; i++){
+//     result *= a;
+// console.log(result);
+// }
+
+
+// for 13
+
+// let n = +prompt("n ni kiriting (n > 0):");
+// let a = +prompt("a ni kiriting ");
+
+// result =1;
+
+// let S = 0;
+
+// for (let i = 1; i <= n; i++){
+//     result *= a;
+//     S +=result;
+// console.log(result);
+
+// }
+
+// console.log(S);
+// for 14
+
+
+
+// for 15
+
+
+
+// for 16
+
+
+
+// for 17
+
+
+// for 18
+
+
+
+// for 19
+
+
+
+// for 20
+
+
